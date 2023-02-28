6341a2bf22a8c6e66634980f03bbac55c4110662
jessica malak <jessimalak21@gmail.com>
2023-02-28 08:30:49 -0500

    add settings to navigation rail

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,9 +1,19 @@
-import 'package:fluent_ui/fluent_ui.dart' hide FluentIcons, ThemeData;
+import 'package:fluent_ui/fluent_ui.dart' hide FluentIcons, ThemeData, IconButton;
 import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 import 'package:flutter/material.dart'
-    show NavigationRail, NavigationRailDestination, TabController, TabBarView, Theme, ThemeData, NavigationRailThemeData;
+    show
+        NavigationRail,
+        NavigationRailDestination,
+        TabController,
+        TabBarView,
+        Theme,
+        ThemeData,
+        NavigationRailThemeData,
+        NavigationRailLabelType,
+        IconButton;
 import 'package:sibupel/screens/home/movies.dart';
 import 'package:sibupel/screens/home/sagas.dart';
+import 'package:sibupel/screens/home/settings.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -17,7 +27,7 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
   late TabController _tabController;
   @override
   void initState() {
-    _tabController = TabController(length: 2, vsync: this);
+    _tabController = TabController(length: 3, vsync: this);
     super.initState();
   }
 
@@ -30,21 +40,33 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
               useMaterial3: true,
               colorSchemeSeed: Colors.teal,
               brightness: Brightness.dark,
-              navigationRailTheme: NavigationRailThemeData(backgroundColor: Colors.grey[170], minWidth: 64)),
+              navigationRailTheme: NavigationRailThemeData(
+                  backgroundColor: Colors.grey[170], minWidth: 64, labelType: NavigationRailLabelType.all, groupAlignment: 0)),
           child: NavigationRail(
-              onDestinationSelected: (value) {
-                _tabController.animateTo(value);
-                setState(() {
-                  _index = value;
-                });
-              },
-              destinations: const [
-                NavigationRailDestination(icon: Icon(FluentIcons.video_clip_20_regular), label: Text("Pelis")),
-                NavigationRailDestination(icon: Icon(FluentIcons.video_clip_multiple_20_regular), label: Text("Sagas"))
-              ],
-              selectedIndex: _index),
+            onDestinationSelected: (value) {
+              _tabController.animateTo(value);
+              setState(() {
+                _index = value;
+              });
+            },
+            destinations: const [
+              NavigationRailDestination(
+                  icon: Icon(FluentIcons.video_clip_20_regular),
+                  selectedIcon: Icon(FluentIcons.video_clip_20_filled),
+                  label: Text("Pelis")),
+              NavigationRailDestination(
+                  icon: Icon(FluentIcons.video_clip_multiple_20_regular),
+                  selectedIcon: Icon(FluentIcons.video_clip_multiple_20_filled),
+                  label: Text("Sagas")),
+              NavigationRailDestination(
+                  icon: Icon(FluentIcons.settings_20_regular),
+                  selectedIcon: Icon(FluentIcons.settings_20_filled),
+                  label: Text("Ajustes"))
+            ],
+            selectedIndex: _index,
+          ),
         ),
-        Expanded(child: TabBarView(controller: _tabController, children: const [MoviesScreen(), SagasScreen()]))
+        Expanded(child: TabBarView(controller: _tabController, children: const [MoviesScreen(), SagasScreen(), SettingsScreen()]))
       ],
     );
   }
